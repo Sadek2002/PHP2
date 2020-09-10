97b4ca2d993041e06aa11caa5083fea1cddf2a66
Sadek2002 <sharkalmousawi@live.com>
2020-09-10 08:53:09 +0200

    ...

---
+++ Hoofdstuk6.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hoofdstuk 6</title>
+</head>
+<body>
+
+<h1>Hoofdstuk 6</h1>
+
+<a href="H6_Opdracht_1.php">Opdracht 1</a>
+
+<br>
+
+<a href="index.php">Terug naar vorige pagina.</a>
